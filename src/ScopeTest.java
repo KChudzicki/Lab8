07a264dfca8ebed7307fa0cac958c556f51f8e37
kchudzic <ckmt.chudzicki@gmail.com>
2022-11-06 16:40:32 +0100

    Initial commit

---
+++ src/ScopeTest.java
@@ -0,0 +1,14 @@
+public class ScopeTest {
+    int test = 10;
+
+    void printTest() {
+        System.out.println("Test: " + test);
+        int test = 20;
+        System.out.println("Test: " + test);
+    }
+
+    public static void main(String[] args) {
+        ScopeTest sc = new ScopeTest();
+        sc.printTest();
+    }
+}
